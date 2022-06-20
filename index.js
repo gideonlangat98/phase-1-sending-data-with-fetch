b98e0192376710182376def18e8dc15dec678921
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-20 18:35:15 +0300

    commit tree 570abf4b6864b39a6b87547c8c65b2dbfc3c15ef

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    }
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then( (resp) => resp.json())
+    .then((data) => { (data)
+
+    document.body.innerHTML = data ["id"]
+    })
+
+    .catch((error) => {(error)
+        document.body.innerHTML = error.message
+    });
+
+}
